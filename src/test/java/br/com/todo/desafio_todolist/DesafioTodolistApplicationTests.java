a8764e5ad179cde772d9af2f78ef4e028d60929d
Eduardo Lima <eduardoaluno1800@gmail.com>
2025-08-11 23:16:36 -0300

    Adicionando construtor padrão para compatibilidade com JPA e padronizando a ordem dos parâmetros dele

--- src/test/java/br/com/todo/desafio_todolist/DesafioTodolistApplicationTests.java
+++ src/test/java/br/com/todo/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -1,13 +1,38 @@
 package br.com.todo.desafio_todolist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import br.com.todo.desafio_todolist.entity.Todo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DesafioTodolistApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
 
+    @Test
+    void testCreateTodo() {
+        Todo todo = new Todo("todo_1", "desc_todo_1", false, 1);
+        
+        webTestClient
+            .post()
+            .uri("/todos")
+            .bodyValue(todo)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody()
+            .jsonPath("$").isArray()
+            .jsonPath("$.length()").isEqualTo(1)
+            .jsonPath("$[0].nome").isEqualTo(todo.getNome())
+            .jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+            .jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+            .jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+    }
+
+    @Test
+    void testCreateTodoFailure() {
+    }
 }
